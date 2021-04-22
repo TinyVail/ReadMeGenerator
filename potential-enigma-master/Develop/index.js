0f2fcec845eb36d8396f8f3975b64366e2460dcd
ClancyGilroy <78936857+TinyVail@users.noreply.github.com>
2021-04-22 17:47:58 -0500

    not sure if these are the right updates or not

--- potential-enigma-master/Develop/index.js
+++ potential-enigma-master/Develop/index.js
@@ -1,13 +1,27 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "list",
+    name: "colorChoice",
+    message: "Choose your favourite color",
+    choices: ["Red", "Orange", "Yellow", "Green"],
+    default: 0
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(`Your favourite Color is ${answers.colorChoice}`);
+        });
+}
 
 // Function call to initialize app
 init();
+
+console.log("i like turtles");
