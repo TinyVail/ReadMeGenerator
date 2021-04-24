f50bb706caef4f0ee1b3503a6d528a7e39359cbd
ClancyGilroy <78936857+TinyVail@users.noreply.github.com>
2021-04-23 19:40:13 -0500

    badges working

--- potential-enigma-master/Develop/utils/generateMarkdown.js
+++ potential-enigma-master/Develop/utils/generateMarkdown.js
@@ -1,14 +1,49 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  const url = `https://img.shields.io/static/v1?label=license&message=${license.replace(new RegExp(" ", "g"), "%20")}&color=green`;
+  return `![license](${url})`;
+}
+
+/**
+Regex Explaination:
+
+const regex = "abcd";
+
+"123abcd".replace(regex, "hi"); -> 123hi
+
+*/
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  const licenseLinks = {
+    "Apache License 2.0": "https://opensource.org/licenses/Apache-2.0",
+    "BSD 3-Clause \"New\" or \"Revised\" license": "https://opensource.org/licenses/BSD-3-Clause",
+    'BSD 2-Clause "Simplified" or "FreeBSD" license': "https://opensource.org/licenses/BSD-2-Clause",
+    'GNU General Public License (GPL)': "https://opensource.org/licenses/gpl-license",
+    'GNU Library or "Lesser" General Public License (LGPL)': "https://opensource.org/licenses/lgpl-license",
+    'MIT license': "https://opensource.org/licenses/lgpl-license",
+    'Mozilla Public License 2.0': "https://opensource.org/licenses/MPL-2.0",
+    'Common Development and Distribution License': "https://opensource.org/licenses/CDDL-1.0",
+    'Eclipse Public License version 2.0': "https://opensource.org/licenses/EPL-2.0"
+  };
+  // If license is Apache License 2.0 then return:
+  // [Apache License 2.0](https://opensource.org/licenses/Apache-2.0)
+  return `[${license}](${licenseLinks[license]})`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if (license === "") {
+    return "";
+  } else {
+    return `${renderLicenseBadge(license)}\n${renderLicenseLink(license)}`;
+  }
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -25,7 +60,7 @@ function generateMarkdown(data) {
   ## Testing Instruction 
   ${data.testInstructions}
   ## Licensing info
-  ${data.license}
+  ${renderLicenseSection(data.license)}
   `;
 }
 
--- potential-enigma-master/README.md
+++ potential-enigma-master/README.md
@@ -1,14 +1,14 @@
-
-  #
+  # fasdsdaf
   ## Description
-
+  sadfDSFGAASDG
   ## Installation Instructions
-
+  SADGGSADGASD
   ## Usage Info
   undefined
   ## Contribution Guidelines
-
+  rgsdaasrgdarsggrasagrs
   ## Testing Instruction
-
+  rgasrsagrseaggasr
   ## Licensing info
-  Apache License 2.0
+  ![license](https://img.shields.io/static/v1?label=license&message=BSD%203-Clause%20"New"%20or%20"Revised"%20license&color=green)
+[BSD 3-Clause "New" or "Revised" license](https://opensource.org/licenses/BSD-3-Clause)
